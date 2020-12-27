78e18b962c1a4592ac7ac99b3f960df3dcd7252b
Mrxxm <362190221@qq.com>
2020-12-27 12:49:56 +0800

    提交

--- foodie-dev-api/src/main/java/com/kenrou/Application.java
+++ foodie-dev-api/src/main/java/com/kenrou/Application.java
@@ -2,8 +2,11 @@ package com.kenrou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+// 扫描 mybatis 通用 mapper 所在的包
+@MapperScan(basePackages = "com.kenrou.mapper")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- foodie-dev-api/src/main/java/com/kenrou/controller/UserController.java
+++ foodie-dev-api/src/main/java/com/kenrou/controller/UserController.java
@@ -1,4 +1,38 @@
 package com.kenrou.controller;
 
+import com.kenrou.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/api/getUser")
+    public Object getUser(String id) {
+
+        return userService.getUser(id);
+    }
+
+    @PostMapping("/api/saveUser")
+    public void saveUser() {
+
+        userService.saveUser();
+    }
+
+    @PostMapping("/api/modifyUser")
+    public void modifyUser(String id) {
+
+        userService.updateUser(id);
+    }
+
+    @PostMapping("/api/deleteUser")
+    public void deleteUser(String id) {
+
+        userService.deleteUser(id);
+    }
 }
