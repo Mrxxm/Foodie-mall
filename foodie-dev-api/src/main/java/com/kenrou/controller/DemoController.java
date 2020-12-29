daec5a306f2ef023f46325feb9239d4d975e8d50
Mrxxm <362190221@qq.com>
2020-12-29 14:52:00 +0800

    回顾session

--- foodie-dev-api/src/main/java/com/kenrou/controller/DemoController.java
+++ foodie-dev-api/src/main/java/com/kenrou/controller/DemoController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @ApiIgnore
 @RestController
 public class DemoController {
@@ -11,5 +14,16 @@ public class DemoController {
     @GetMapping("/api/demo/index")
     public Object index() {
         return "Hello demo index";
+    }
+
+    @GetMapping("/setSession")
+    public Object setSession(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.setAttribute("userInfo", "new user");
+        session.setMaxInactiveInterval(3600);
+        session.getAttribute("userInfo");
+        session.removeAttribute("userInfo");
+
+        return "ok";
     }
 }
