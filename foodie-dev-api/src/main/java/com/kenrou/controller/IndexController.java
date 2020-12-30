243fa65afaa85c7819708c3bd88f12a14c455250
Mrxxm <362190221@qq.com>
2020-12-30 10:33:36 +0800

    轮播图接口

--- foodie-dev-api/src/main/java/com/kenrou/controller/IndexController.java
+++ foodie-dev-api/src/main/java/com/kenrou/controller/IndexController.java
@@ -1,15 +1,17 @@
 package com.kenrou.controller;
 
+import com.kenrou.enums.YesOrNo;
+import com.kenrou.pojo.Carousel;
 import com.kenrou.service.CarouselService;
 import com.kenrou.utils.IMOOCJSONResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Api(value = "首页", tags = {"首页相关接口"})
 @RestController
@@ -19,21 +21,10 @@ public class IndexController {
     @Autowired
     private CarouselService carouselService;
 
-    @ApiOperation(value = "用户名是否存在", notes = "用户名是否存在", httpMethod = "GET")
-    @GetMapping("/usernameIsExist")
-    public IMOOCJSONResult usernameIsExist(@RequestParam String username) {
-
-        // 1.判断入参是否为空
-        if (StringUtils.isBlank(username)) {
-            return IMOOCJSONResult.errorMsg("用户名不能为空");
-        }
-        // 2.查找username
-        boolean result = userService.queryUsernameIsExist(username);
-
-        if (result) {
-            return IMOOCJSONResult.errorMsg("用户名已经存在");
-        }
-        // 3.请求成功，用户名没有重复
-        return IMOOCJSONResult.ok();
+    @ApiOperation(value = "轮播图列表", notes = "轮播图列表", httpMethod = "GET")
+    @GetMapping("/carousel")
+    public IMOOCJSONResult carousel() {
+        List<Carousel> result = carouselService.queryAll(YesOrNo.YES.type);
+        return IMOOCJSONResult.ok(result);
     }
 }
