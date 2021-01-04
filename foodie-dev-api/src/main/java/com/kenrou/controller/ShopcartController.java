b46c5f935f3e313670c30f40c5b3d102754cf72c
Mrxxm <362190221@qq.com>
2021-01-04 10:30:39 +0800

    删除购物车商品接口添加

--- foodie-dev-api/src/main/java/com/kenrou/controller/ShopcartController.java
+++ foodie-dev-api/src/main/java/com/kenrou/controller/ShopcartController.java
@@ -33,4 +33,22 @@ public class ShopcartController {
 
         return IMOOCJSONResult.ok();
     }
+
+    @ApiOperation(value = "删除购物车商品", notes = "删除购物车商品", httpMethod = "POST")
+    @PostMapping("/del")
+    public IMOOCJSONResult del(
+            @RequestParam String userId,
+            @RequestParam String itemSpecId,
+            HttpServletRequest request,
+            HttpServletResponse response) {
+
+        if (StringUtils.isBlank(userId)
+        || StringUtils.isBlank(itemSpecId)) {
+            IMOOCJSONResult.errorMsg("参数不能为空");
+        }
+
+        // TODO 用户在页面删除 购物车中的商品，如果用户已登录，需要同时在后端删除购物车商品到redis
+
+        return IMOOCJSONResult.ok();
+    }
 }
