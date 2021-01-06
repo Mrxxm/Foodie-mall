032d93c9a5706ae2a3db5d025533149e49f0acda
Mrxxm <362190221@qq.com>
2021-01-06 15:55:05 +0800

    创建订单

--- foodie-dev-api/src/main/java/com/kenrou/controller/OrdersController.java
+++ foodie-dev-api/src/main/java/com/kenrou/controller/OrdersController.java
@@ -2,6 +2,7 @@ package com.kenrou.controller;
 
 import com.kenrou.enums.PayMethod;
 import com.kenrou.pojo.bo.SubmitOrderBO;
+import com.kenrou.service.OrderService;
 import com.kenrou.utils.IMOOCJSONResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -16,16 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("orders")
 public class OrdersController {
 
+    @Autowired
+    private OrderService orderService;
+
     @ApiOperation(value = "用户下单", notes = "用户下单", httpMethod = "POST")
     @PostMapping("/create")
     public IMOOCJSONResult create(@RequestBody SubmitOrderBO submitOrderBO) {
 
         if (submitOrderBO.getPayMethod() != PayMethod.WEIXIN.type
-                || submitOrderBO.getPayMethod() != PayMethod.ALIPAY.type) {
+                && submitOrderBO.getPayMethod() != PayMethod.ALIPAY.type) {
             return IMOOCJSONResult.errorMsg("支付方式类型错误");
         }
 
         // 1.创建订单
+        orderService.createOrder(submitOrderBO);
         // 2.移除购物车中以提交的商品
         // 3.向支付中心发起请求
 
