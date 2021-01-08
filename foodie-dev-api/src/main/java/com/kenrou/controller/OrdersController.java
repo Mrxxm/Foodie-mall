cb7cb465ba4b07ade4aec578a627c2f49048ebec
Mrxxm <362190221@qq.com>
2021-01-08 15:17:45 +0800

    提交

--- foodie-dev-api/src/main/java/com/kenrou/controller/OrdersController.java
+++ foodie-dev-api/src/main/java/com/kenrou/controller/OrdersController.java
@@ -10,10 +10,8 @@ import com.kenrou.pojo.vo.OrderVo;
 import com.kenrou.service.OrderService;
 import com.kenrou.utils.CookieUtils;
 import com.kenrou.utils.IMOOCJSONResult;
-import com.kenrou.utils.MD5Utils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import springfox.documentation.spring.web.json.Json;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
