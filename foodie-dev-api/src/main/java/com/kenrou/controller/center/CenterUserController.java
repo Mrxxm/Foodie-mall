8c6bafdce4e48af0dd459241de0c1ba2fc8ede9d
Mrxxm <362190221@qq.com>
2021-01-11 19:59:45 +0800

    hibernate 验证

--- foodie-dev-api/src/main/java/com/kenrou/controller/center/CenterUserController.java
+++ foodie-dev-api/src/main/java/com/kenrou/controller/center/CenterUserController.java
@@ -10,10 +10,16 @@ import com.kenrou.utils.JsonUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Api(value = "我的信息", tags = {"我的信息"})
 @RestController
@@ -27,15 +33,36 @@ public class CenterUserController extends BaseController {
     @ApiOperation(value = "更新用户信息", notes = "更新用户信息", httpMethod = "POST")
     @PostMapping("update")
     public IMOOCJSONResult update(@RequestParam String userId,
-                                  @RequestBody CenterUserBO centerUserBO,
+                                  @RequestBody @Valid CenterUserBO centerUserBO,
+                                  BindingResult bindingResult,
                                   HttpServletRequest request, HttpServletResponse response) {
-        Users user = centerUserService.updateUserInfo(userId, centerUserBO);
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorMap = getErrors(bindingResult);
+            return IMOOCJSONResult.errorMap(errorMap);
+        }
 
+        Users user = centerUserService.updateUserInfo(userId, centerUserBO);
         Users handleUser = setNullProperty(user);
 
         // 设置cookie
         CookieUtils.setCookie(request, response, "user", JsonUtils.objectToJson(handleUser), true);
 
+        // TODO 后续要改，增加令牌token，整合redis
+
         return IMOOCJSONResult.ok();
+    }
+
+    private Map<String, String> getErrors(BindingResult result) {
+
+        Map<String, String> map = new HashMap<>();
+        List<FieldError> errorList = result.getFieldErrors();
+        for (FieldError error: errorList) {
+            String errorFiled = error.getField();
+            String errorMsg = error.getDefaultMessage();
+
+            map.put(errorFiled, errorMsg);
+        }
+
+        return map;
     }
 }
--- foodie-dev-pojo/src/main/java/com/kenrou/pojo/bo/center/CenterUserBO.java
+++ foodie-dev-pojo/src/main/java/com/kenrou/pojo/bo/center/CenterUserBO.java
@@ -1,20 +1,36 @@
 package com.kenrou.pojo.bo.center;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.*;
 import java.util.Date;
 
 @ApiModel(value = "用户中心对象BO", description = "从客户端，由用户传入的数据封装在此entity中")
 public class CenterUserBO {
 
-//    @ApiModelProperty(value = "用户名", name = "username", example = "kenrou", required = true)
+    @ApiModelProperty(value = "用户名", name = "username", example = "kenrou", required = true)
     private String username;
     private Date birthday;
+
+    @Email
     private String email;
+
     private String face;
+    @Pattern(regexp = "^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\\d{8})$", message = "手机号格式错误")
     private String mobile;
+
+    @NotBlank(message = "昵称不能为空")
+    @Length(max = 12, message = "用户昵称不能超过12位")
     private String nickname;
+
+    @NotBlank(message = "姓名不能为空")
+    @Length(max = 12, message = "姓名不能超过12位")
     private String realname;
+
+    @Min(value = 0, message = "性别选择不正确")
+    @Max(value = 2, message = "性别选择不正确")
     private Integer sex;
 
     public String getUsername() {
