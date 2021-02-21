90df6f9aa28c6adcb0b2f145d5ef28c057677947
Mrxxm <362190221@qq.com>
2021-02-21 16:21:57 +0800

    提交

--- foodie-dev-api/src/main/java/com/kenrou/Application.java
+++ foodie-dev-api/src/main/java/com/kenrou/Application.java
@@ -2,12 +2,13 @@ package com.kenrou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 // 扫描 mybatis 通用 mapper 所在的包
 @MapperScan(basePackages = "com.kenrou.mapper")
 // 扫描所有包以及相关组件包
--- foodie-dev-search/src/main/java/com/kenrou/Application.java
+++ foodie-dev-search/src/main/java/com/kenrou/Application.java
@@ -2,10 +2,6 @@ package com.kenrou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 public class Application {
